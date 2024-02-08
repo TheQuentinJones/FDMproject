30afd9fae456487c43eabf61147f4525f9c84722
Quentin Jones <qj82189@gmail.com>
2024-02-08 17:26:45 -0500

    added some javascript

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const buttonSubmit = (e) => {
+    e.preventDefault()
+    try {
+    let a = document.getElementById("inputGroupSelect01")
+    let b = document.getElementById("dollarAmount")
+    let x = a.value
+    let y = b.value
+
+    console.log("Type of Account: " + x + ", Dollar Amount Input: " + y)    
+
+    let answer = confirm("Success!! Add another?")
+    
+    if(!answer){
+    window.location.href = "./index.html"
+    } else if (answer) {
+        b.value = ""
+    }
+
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const theButton = document.getElementById("primary-submit")
+
+theButton.addEventListener('click', buttonSubmit)
